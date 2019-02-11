997241a4566ac46455b673e1cd3803b5ed140572
mallikarjun-peasarla <mallikarjuna.peasarla@cotiviti.com>
2019-02-11 20:17:59 +0530

    5. Strictness and Laziness - Stream 2

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -12,19 +12,24 @@ trait Stream[+A] {
   def exists(p: A => Boolean): Boolean = 
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
+  def exists1(p: A => Boolean): Boolean = this match {
+    case Cons(h, t) => p(h()) || t().exists(p)
+    case _ => false
+  }
+
   @annotation.tailrec
   final def find(f: A => Boolean): Option[A] = this match {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
 
-//  def toList: List[A] = {
-//    def go(s: => Stream[A]): List[A] = this match {
-//      case Empty => List()
-//      case Cons(h, t) => h() :: go(t())
-//    }
-//    go(this)
-//  }
+  def toList1: List[A] = {
+    def go(s: => Stream[A]): List[A] = s match {
+      case Empty => List()
+      case Cons(h, t) => h() :: go(t())
+    }
+    go(this)
+  }
 
   def toList: List[A] = this match {
     case Empty =>
@@ -33,24 +38,39 @@ trait Stream[+A] {
       h() :: t().toList
   }
 
-  def take(n: Int): Stream[A] = {
-    def go(s: Stream[A], n: Int): Stream[A] = this match {
-      case Empty => Empty
-      case Cons(h, t) => cons[A](h(), go(t(), n-1))
-    }
-    go(this, n)
+  def take(n: Int): Stream[A] = this match {
+    case Cons(_, _) if(n == 0) => Empty
+    case Cons(h, t) => cons[A](h(), t().take(n-1))
   }
 
-  def drop(n: Int): Stream[A] = ???
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if(n == 0) => cons[A](h(), t())
+    case Cons(h, t) => t().drop(n-1)
+  }
 
-  def takeWhile(p: A => Boolean): Stream[A] = ???
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if(p(h())) => cons[A](h(), t().takeWhile(p))
+    case Cons(h, t) => Empty
+  }
 
-  def forAll(p: A => Boolean): Boolean = ???
+  // f: (A, => B) => B
+  //todo: terminate when function not true ?
+  def takeWhile2(p: A => Boolean): Stream[A] = foldRight(Stream[A]())((a, b) => if(p(a)) cons(a, b) else b)
 
-  def headOption: Option[A] = ???
+  def forAll1(p: A => Boolean): Boolean = this.foldRight(true)((e,a) => p(e) && a)
 
-  // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
-  // writing your own function signatures.
+  def forAll(p: A => Boolean): Boolean = this match {
+    case Cons(h, t) if(p(h())) => true && t().forAll(p)
+    case Empty => true
+    case _ => false
+  }
+
+  def headOption: Option[A] = foldRight(None: Option[A]){
+    case (a: A, _) => Some(a)
+  }
+
+  // todo: 5.7 map, filter, append, flatmap using foldRight.
+  // Part of the exercise is writing your own function signatures.
 
   def startsWith[B](s: Stream[B]): Boolean = ???
 }
@@ -98,7 +118,15 @@ object Stream {
     // Streams
     val stream1 = Stream.apply(1,2,3,4)
     println("stream: "+stream1)
-    println("stream.toList: "+stream1.toList)
+    println("stream.toList: "+stream1.toList1)
     println("stream.take(3).toList: "+stream1.take(3).toList)
+    println("stream.drop(3).toList: "+stream1.drop(3).toList)
+    println("stream.drop(2).toList: "+stream1.drop(2).toList)
+    println("stream.takeWhile(_ < 3).toList: "+stream1.takeWhile(_ < 3).toList)
+    println("stream.takeWhile2(_ < 3).toList: "+stream1.takeWhile2(_ < 3).toList)
+    println("stream.forAll(_ < 5): "+stream1.forAll(_ < 5))
+    println("stream.forAll(_ % 2 == 0): "+stream1.forAll(_ % 2 == 0))
+    println("stream.headOption: "+stream1.headOption)
+    println("emptyStream.headOption: "+Stream.apply().headOption)
   }
 }
