c5dc1ac2b42c5f6a98a253af474d75e77f467ecc
mallikarjun-peasarla <mallikarjuna.peasarla@cotiviti.com>
2019-02-11 14:54:25 +0530

    7. Puerly functional Parallelism - Par 1 - initial

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -9,6 +9,8 @@ object Par {
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
   
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true 
@@ -21,8 +23,10 @@ object Par {
     (es: ExecutorService) => {
       val af = a(es) 
       val bf = b(es)
-      UnitFuture(f(af.get, bf.get)) // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures. This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
+      UnitFuture(f(af.get(100, TimeUnit.MILLISECONDS), bf.get(100, TimeUnit.MILLISECONDS))) // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures. This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
     }
+
+  def asyncF[A,B](f: A => B): A => Par[B] = (a: A) => (es: ExecutorService) => UnitFuture(f(a))
   
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
     es => es.submit(new Callable[A] { 
@@ -32,7 +36,33 @@ object Par {
   def map[A,B](pa: Par[A])(f: A => B): Par[B] = 
     map2(pa, unit(()))((a,_) => f(a))
 
+  def lift[A, B](f: A => B): Par[A] => Par[B] = (a: Par[A]) => map(a)(a => f(a))
+
   def sortPar(parList: Par[List[Int]]) = map(parList)(_.sorted)
+
+//  def parMap1[A,B](ps: List[A])(f: A => B): Par[List[B]] = {
+//    val bList = ps.map(a => f(a))
+//    lazyUnit(bList)
+//  }
+
+  def parMap2[A,B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+    val fn = (la: List[A]) => la.map(a => f(a))
+    asyncF(fn)(ps)
+  }
+
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] = {
+    es:ExecutorService => UnitFuture(ps.map{parA: Par[A] => parA(es).get})
+  }
+
+  def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+    val fbs: List[Par[B]] = ps.map(asyncF(f))
+    sequence(fbs)
+  }
+
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = fork {
+    val filtered = as.filter(f)
+    unit(filtered)
+  }
 
   def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean = 
     p(e).get == p2(e).get
